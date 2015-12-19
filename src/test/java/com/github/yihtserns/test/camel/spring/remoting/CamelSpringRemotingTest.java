10f7472b688e88401ab711a965a549f7b6d2c6fc
yihtserns <sim_yihtsern@yahoo.com>
2015-12-19 17:54:31 +0800

    Initial working version.

---
+++ src/test/java/com/github/yihtserns/test/camel/spring/remoting/CamelSpringRemotingTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2015 yihtserns.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.yihtserns.test.camel.spring.remoting;
+
+import com.github.yihtserns.test.camel.spring.remoting.testutil.Service;
+import com.github.yihtserns.test.camel.spring.remoting.testutil.Request;
+import com.github.yihtserns.test.camel.spring.remoting.testutil.Response;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.junit.Test;
+import org.springframework.remoting.httpinvoker.HttpInvokerProxyFactoryBean;
+import org.springframework.remoting.support.RemoteInvocationResult;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.After;
+import org.springframework.remoting.support.RemoteInvocation;
+
+/**
+ * @author yihtserns
+ */
+public class CamelSpringRemotingTest {
+
+    private DefaultCamelContext camelContext = new DefaultCamelContext();
+
+    @After
+    public void stopCamelContext() throws Exception {
+        if (camelContext.isStarted()) {
+            camelContext.stop();
+        }
+    }
+
+    @Test
+    public void canServeSpringRemotingCallerUsingCamelJetty() throws Exception {
+        final String url = "http://localhost:8088/trigger";
+
+        camelContext.addRoutes(new RouteBuilder() {
+
+            @Override
+            public void configure() throws Exception {
+                from("jetty:" + url)
+                        .process(new Processor() {
+
+                            @Override
+                            public void process(Exchange exchange) throws Exception {
+                                RemoteInvocation remoteInvocation = exchange.getIn().getBody(RemoteInvocation.class);
+                                Request req = (Request) remoteInvocation.getArguments()[0];
+                                String reply = req.message + " Bye!";
+
+                                exchange.getIn().setBody(new RemoteInvocationResult(new Response(reply)));
+                            }
+                        });
+            }
+        });
+        camelContext.start();
+
+        HttpInvokerProxyFactoryBean factoryBean = new HttpInvokerProxyFactoryBean();
+        factoryBean.setServiceInterface(Service.class);
+        factoryBean.setServiceUrl(url);
+        factoryBean.afterPropertiesSet();
+        Service service = (Service) factoryBean.getObject();
+
+        Response response = service.service(new Request("Hi!"));
+        assertThat(response, is(notNullValue()));
+        assertThat(response.message, is("Hi! Bye!"));
+    }
+}
---
+++ src/test/java/com/github/yihtserns/test/camel/spring/remoting/testutil/Request.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2015 yihtserns.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.yihtserns.test.camel.spring.remoting.testutil;
+
+import java.io.Serializable;
+
+/**
+ * @author yihtserns
+ */
+public class Request extends Envelope implements Serializable {
+
+    /**
+     * For serializers.
+     */
+    public Request() {
+    }
+
+    public Request(String message) {
+        super(message);
+    }
+}
